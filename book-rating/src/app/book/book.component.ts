35aa10aab1edf3b8683094074c02cbe76402e04d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-09-18 17:43:18 +0200

    Directives and Components

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent implements OnInit {
+
+  @Input() book: Book;
+
+  ngOnInit() {
+  }
+
+}
--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books: string[];
+  books: Book[];
 
   constructor() { }
 
   ngOnInit() {
-    this.books = ['Angular', 'AngularJS'];
+    this.books = [{
+      isbn: '000',
+      title: 'Angular',
+      description: 'Tolles Buch',
+      rating: 5
+    },
+    {
+      isbn: '111',
+      title: 'AngularJS',
+      description: 'Altes Buch',
+      rating: 3
+    }
+  ];
   }
-
 }
