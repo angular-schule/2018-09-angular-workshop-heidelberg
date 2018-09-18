ab0d3db9eb79c2a2eb93db6f4c43dcf217abefcd
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-09-18 21:56:47 +0200

    fun with SCSS

--- book-rating/src/app/book/book.component.ts
+++ book-rating/src/app/book/book.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class BookComponent implements OnInit {
 
