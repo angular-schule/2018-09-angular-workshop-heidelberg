b7a9b1d9b13292ff78c84769403118e87d7c9df8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-09-20 11:26:00 +0200

    playing with ChangeDetectionStrategy.OnPush

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush // VORSICHT: funktioniert bei AJAX nicht mehr
 })
 export class DashboardComponent implements OnInit {
 
