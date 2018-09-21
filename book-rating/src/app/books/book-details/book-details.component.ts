f969556c2051a9d842d9e97518c8223d568d9a74
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-09-21 11:30:59 +0200

    introducing mergeMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
 import { Observable, of, from, timer } from 'rxjs';
-import { map, filter, reduce } from 'rxjs/operators';
+import { map, filter, reduce, tap, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'br-book-details',
@@ -18,35 +18,12 @@ export class BookDetailsComponent implements OnInit {
     private bookStore: BookStoreService) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
 
-      const isbn = paramMap.get('isbn');
+    this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('isbn')),
+      mergeMap(isbn => this.bookStore.getSingle(isbn)),
+      tap(x => console.log(x))
+    ).subscribe(book => this.book = book);
 
-      // via callbacks
-      this.bookStore.getSingle(isbn).subscribe(
-        book => this.book = book,
-        error => console.error('Es gab nen Fehler', error),
-        () => console.log('Nr. 1 Complete! 😀')
-      );
-
-      // via observer
-      const observer = {
-        next: book => console.log('Hui ein Buch', book),
-        error: error => console.error('Es gab nen Fehler', error),
-        complete: () => console.log('Nr. 2 Complete! 😀')
-      };
-
-      const myObservable$ = from([1, 2, 3, 4, 5]).
-        pipe(
-          filter(n => n % 2 === 0),
-          map(n => n + 10),
-          reduce((acc, item) => acc + item, 0)
-        );
-
-
-      myObservable$.subscribe(observer);
-
-
-     });
   }
 }
